c7b1595446593f1f3ee9011eec92e12f5e2bf173
FreshMurry <lawrencemurry@yahoo.com>
2017-07-06 17:32:12 +0000

    Extra tab

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,5 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update]
-  before_action :authenticate_user!, except: [:show]
 
   def index
     @rooms = current_user.rooms
@@ -24,7 +23,7 @@ class RoomsController < ApplicationController
 
     if @room.save
 
-      if params[:images] 
+      if params[:images]
         params[:images].each do |image|
           @room.photos.create(image: image)
         end
@@ -48,24 +47,27 @@ class RoomsController < ApplicationController
   def update
     if @room.update(room_params)
 
-      if params[:images] 
-        params[:images].each do |image|
-          @room.photos.create(image: image)
+      if @room.save
+        if params[:images]
+          params[:images].each do |image|
+            @room.photos.create(image: image)
+          end
         end
-      end
 
       redirect_to edit_room_path(@room), notice: "Updated..."
-    else
-      render :edit
+      else
+        render :edit
+      end
     end
   end
 
   private
     def set_room
-      @room = Room.find(params[:id]) 
+      @room = Room.find(params[:id])
     end
 
     def room_params
       params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active)
+
     end
 end
--- db/migrate/20151026130045_create_rooms.rb
+++ db/migrate/20151026130045_create_rooms.rb
@@ -1,6 +1,6 @@
 class CreateRooms < ActiveRecord::Migration
   def change
-    create_table :rooms do |t|
+      create_table :rooms do |t|
       t.string :home_type
       t.string :room_type
       t.integer :accommodate
