daca5c3fc9993faf11287369986b4f05e4e9412a
FreshMurry <lawrencemurry@yahoo.com>
2017-09-21 04:55:24 +0000

    Adding back the devise parameter for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer(:sign_up) << :fullname
-      devise_parameter_sanitizer(:account_update) << :fullname << :phone_number << :description << :email << :password
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
+      devise_parameter_sanitizer.for(:account_update) << :fullname << :phone_number << :description << :email << :password
     end
 end
 
